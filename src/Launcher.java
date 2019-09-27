4efd6b477646b6e65fd8bfe52ebebfa894a6680e
egoing <egoing@gmail.com>
2019-09-27 09:29:07 +0900

    Tomcat web container 설정
    이 단계에서는 서버를 생성하고,
    서버의 포트를 지정하고,
    서버를 실행시키고,
    서버를 대기시키는 작업이 필요합니다.

---
+++ src/Launcher.java
@@ -0,0 +1,23 @@
+import java.io.File;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
+public class Launcher {
+
+	public static void main(String[] args) throws LifecycleException {
+		
+		Tomcat server = new Tomcat();
+		
+		Context app = server.addWebapp("/app", new File("WebContent").getAbsolutePath());
+		Tomcat.addServlet(app, "read-servlet", new ReadServlet());
+		app.addServletMappingDecoded("/read", "read-servlet");
+		
+		server.setPort(9999);
+		server.start();
+		server.getServer().await();
+
+	}
+
+}
