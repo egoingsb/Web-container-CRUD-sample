e9c20cc7ef3a07dd87c466ffecc9974d10ade970
egoing <egoing@gmail.com>
2019-09-27 12:22:45 +0900

    기본 웹페이지 적용

--- src/ReadServlet.java
+++ src/ReadServlet.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Random;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -11,7 +12,19 @@ public class ReadServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		PrintWriter pw = resp.getWriter();
-		pw.write("Hello World Servlet");
+		pw.write(""
+				+ "<html>"
+				+ "	<body>"
+				+ "		<h1>WEB</h1>"
+				+ "		<ol>"
+				+ "			<li><a href=\"read?id=HTML\">HTML</a></li>"
+				+ "			<li><a href=\"read?id=CSS\">CSS</a></li>"
+				+ "			<li><a href=\"read?id=JavaScript\">JavaScript</a></li>"
+				+ "		</ol>"
+				+ "		<h2>HTML</h2>"
+				+ "		HTML is ..."
+				+ "	</body>"
+				+ "</html>");
 		pw.close();
 	}
 
